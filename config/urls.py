f1303e5ab49392cf1118a105db286a31fb6017e8
varunsha30 <varun141098@gmail.com>
2021-04-24 19:18:11 +0530

    added cheese list view and page, added navbar link for cheeses

--- config/urls.py
+++ config/urls.py
@@ -16,15 +16,19 @@ urlpatterns = [
         TemplateView.as_view(template_name="pages/about.html"),
         name="about",
     ),
+
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
+
     # User management
     path(
         "users/",
         include("everycheese.users.urls", namespace="users"),
     ),
     path("accounts/", include("allauth.urls")),
+
     # Your stuff: custom urls includes go here
+    path('cheeses/', include('everycheese.cheeses.urls', namespace='cheeses')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
