eccf3206865ffc9371ff977d31fea6c6979cc70c
varunsha30 <varun141098@gmail.com>
2021-05-24 14:43:52 +0530

    login requirement to add cheeses

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Cheese
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class CheeseListView(ListView):
     model = Cheese
 
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
     fields = ['name', 'description', 'firmness', 'country']
 
